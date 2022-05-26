93534b11aafc679197c333023af7858a4797f040
AnmolSharma0001 <anmol.as2002@gmail.com>
2022-05-27 00:52:46 +0530

    Adding JS and sounds

---
+++ index.js
@@ -0,0 +1,35 @@
+document.querySelector(".w").addEventListener("click", function () {
+    const newLocal = "sounds/tom-1.mp3";
+    var audio = new Audio(newLocal);
+    audio.play();
+});
+document.querySelector(".a").addEventListener("click", function () {
+    const newLocal = "sounds/tom-2.mp3";
+    var audio = new Audio(newLocal);
+    audio.play();
+});
+document.querySelector(".s").addEventListener("click", function () {
+    const newLocal = "sounds/crash.mp3";
+    var audio = new Audio(newLocal);
+    audio.play();
+});
+document.querySelector(".d").addEventListener("click", function () {
+    const newLocal = "sounds/tom-3.mp3";
+    var audio = new Audio(newLocal);
+    audio.play();
+});
+document.querySelector(".j").addEventListener("click", function () {
+    const newLocal = "sounds/tom-4.mp3";
+    var audio = new Audio(newLocal);
+    audio.play();
+});
+document.querySelector(".k").addEventListener("click", function () {
+    const newLocal = "sounds/kick-bass.mp3";
+    var audio = new Audio(newLocal);
+    audio.play();
+});
+document.querySelector(".l").addEventListener("click", function () {
+    const newLocal = "sounds/snare.mp3";
+    var audio = new Audio(newLocal);
+    audio.play();
+});
