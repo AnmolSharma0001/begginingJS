a957b76e030be5fbded871637f44eb0153352a98
AnmolSharma0001 <anmol.as2002@gmail.com>
2022-06-01 02:30:26 +0530

    Adding sounds on keydown

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
+function playsounds(st) {
+    switch (st) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            console.log("Press valid Key");
+            break;
 
+    }
+}
 
-for(var i=0;i<7;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        var buttoninhtml=this.innerHTML;
-        switch(buttoninhtml){
-            case "w":
-                var audio=new Audio("sounds/tom-1.mp3");
-                audio.play();
-                break;
-            case "a":
-                var audio=new Audio("sounds/tom-2.mp3");
-                audio.play();
-                break;
-            case "s":
-                var audio=new Audio("sounds/tom-3.mp3");
-                audio.play();
-                break;
-            case "d":
-                var audio=new Audio("sounds/tom-4.mp3");
-                audio.play();
-                break;
-            case "j":
-                var audio=new Audio("sounds/snare.mp3");
-                audio.play();
-                break;
-            case "k":
-                var audio=new Audio("sounds/crash.mp3");
-                audio.play();
-                break;
-            case "l":
-                var audio=new Audio("sounds/kick-base.mp3");
-                audio.play();
-                break;
-            default:
-                var audio=new Audio("sounds/tom-1.mp3");
-                audio.play();
-                break;
-                
-        }
+for (var i = 0; i < 7; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttoninhtml = this.innerHTML;
+        playsounds(buttoninhtml);
     });
 }
+
+document.addEventListener("keydown", function (event) {
+    //console.log(event);
+    playsounds(event.key);
+})
